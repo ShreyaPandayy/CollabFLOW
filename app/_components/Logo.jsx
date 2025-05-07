00693f6046752ed2ee3e97a21100f712eea01a50
Shreya Pandey <shreeyapandey456@gmail.com>
2025-05-07 13:15:34 +0530

    Updated name and UI text to CollabFLOW

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -4,12 +4,11 @@ import React from 'react'
 
 function Logo() {
   return (
-    <Link href={'/dashboard'} className='flex items-center gap-2'>
-        <Image src={'/logo.png'} alt='logo'
-        width={30} height={30} />
-        <h2 className='font-bold text-xl'>Loop</h2>
+    <Link href={"/dashboard"} className="flex items-center gap-2">
+      <Image src={"/logo.png"} alt="logo" width={30} height={30} />
+      <h2 className="font-bold text-xl">CollabFLOW</h2>
     </Link>
-  )
+  );
 }
 
 export default Logo
